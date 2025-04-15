a26d45e6db8fc559911c226eb0a25f15e7d094ff
BLisme <tranbaolam006@gmail.com>
2025-04-15 22:15:34 +0800

    3 default file for website

---
+++ js-file.js
@@ -0,0 +1,15 @@
+function add(x,y){
+    return x+y;
+}
+function subtract(x,y){
+    return x-y;
+}
+function mutiple(x,y){
+    return x*y;
+}
+function divide(x,y){
+    return x/y;
+}
+
+let userNum1,userNum2, userOperator;
+
